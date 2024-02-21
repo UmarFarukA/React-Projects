a9fdf135ed6f630cdcac0280582d9db35230827c
UmarFarukA <talk2ufaz@gmail.com>
2024-02-21 10:24:05 +0100

    update:ui

--- the-wild-oasis/src/ui/MainNav.jsx
+++ the-wild-oasis/src/ui/MainNav.jsx
@@ -7,33 +7,48 @@ import { NavLink } from "react-router-dom";
 
 export default function MainNav() {
   return (
-    <ul className="pl-4 flex flex-col items-start gap-8">
-      <li className="hover:text-stone-400 hover:transition-all">
-        <NavLink to="dashboard" className="flex items-center gap-3">
-          <IoHomeOutline />
+    <ul className="pl-4 flex flex-col items-start gap-8 text-md">
+      <li className="hover:text-stone-400 transition-all hover:ease-linear hover:delay-100 hover:translate-x-2 hover:scale-110">
+        <NavLink
+          to="dashboard"
+          className="flex items-center gap-3 hover:text-purple-600"
+        >
+          <IoHomeOutline className="" />
           <span>Home</span>
         </NavLink>
       </li>
       <li className="hover:text-stone-400 hover:transition-all">
-        <NavLink to="booking" className="flex items-center gap-3">
+        <NavLink
+          to="booking"
+          className="flex items-center gap-3 hover:text-purple-600"
+        >
           <SlCalender />
           <span>Booking</span>
         </NavLink>
       </li>
       <li className="hover:text-stone-400 hover:transition-all">
-        <NavLink to="cabin" className="flex items-center gap-3">
+        <NavLink
+          to="cabin"
+          className="flex items-center gap-3 hover:text-purple-600"
+        >
           <MdCabin />
           <span>Cabin</span>
         </NavLink>
       </li>
       <li className="hover:text-stone-400 hover:transition-all">
-        <NavLink to="users" className="flex items-center gap-3">
+        <NavLink
+          to="users"
+          className="flex items-center gap-3 hover:text-purple-600"
+        >
           <PiUsersThreeThin />
           <span>Users</span>
         </NavLink>
       </li>
       <li className="hover:text-stone-400 hover:transition-all">
-        <NavLink to="settings" className="flex items-center gap-3">
+        <NavLink
+          to="settings"
+          className="flex items-center gap-3 hover:text-purple-600"
+        >
           <IoSettingsOutline />
           <span>Settings</span>
         </NavLink>
