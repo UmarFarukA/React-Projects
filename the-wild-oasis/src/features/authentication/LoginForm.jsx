adbe24e7b4b6d48491c45983b7b491ad33dafbc1
UmarFarukA <talk2ufaz@gmail.com>
2024-04-25 15:42:02 +0100

    update

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -1,40 +1,57 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState("user@example.com");
+  const [password, setPassword] = useState("pass@123");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
+  };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
-        <Input
-          type="email"
-          id="email"
-          // This makes this form better for password managers
-          autoComplete="username"
+    <form className="mx-5 sm:mx-8 md:mx-12" onSubmit={handleClick}>
+      <div className="flex flex-col gap-2 mt-2 mb-4">
+        <label className="text-stone-700">Email address</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email address"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          className="bg-stone-200 px-1 py-2 rounded-md outline-none focus:ring focus:ring-stone-600"
+          disabled={isLoading}
         />
-      </FormRowVertical>
-      <FormRowVertical label="Password">
-        <Input
+      </div>
+      <div className="flex flex-col gap-2 mt-2 mb-4">
+        <label className="text-stone-700">Password</label>
+        <input
           type="password"
-          id="password"
-          autoComplete="current-password"
+          name="password"
+          placeholder="***********"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className="bg-stone-200 px-1 py-2 rounded-md outline-none focus:ring focus:ring-stone-600"
+          disabled={isLoading}
         />
-      </FormRowVertical>
-      <FormRowVertical>
-        <Button size="large">Login</Button>
-      </FormRowVertical>
-    </Form>
+      </div>
+      <Button type="stretch" disabled={isLoading}>
+        {isLoading ? "Authenticating..." : "Login"}
+      </Button>
+    </form>
   );
 }
 
--- the-wild-oasis/src/pages/Login.jsx
+++ the-wild-oasis/src/pages/Login.jsx
@@ -1,62 +1,14 @@
-import { useState } from "react";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Logo from "../ui/Logo";
-import { useLogin } from "../features/authentication/useLogin";
+import LoginForm from "../features/authentication/LoginForm";
 
 function Login() {
-  const { login, isLoading } = useLogin();
-
-  const [email, setEmail] = useState("user@example.com");
-  const [password, setPassword] = useState("pass@123");
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    if (!email || !password) return;
-    login(
-      { email, password },
-      {
-        onSettled: () => {
-          setEmail("");
-          setPassword("");
-        },
-      }
-    );
-  };
-
   return (
-    <div className="flex flex-col justify-center w-full m-auto sm:w-2/5 md:w-1/3 pt-8 space-y-6">
+    <div className="flex flex-col justify-center m-auto w-full sm:w-2/3 md:w-2/3 lg:w-2/5  pt-8 space-y-6">
       <Logo />
       <Heading type="h4">Log in to your account</Heading>
-      <form className="m-auto" onSubmit={handleClick}>
-        <div className="flex flex-col gap-2 mt-2 mb-4">
-          <label className="text-stone-700">Email address</label>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-stone-200 px-1 py-2 rounded-md outline-none focus:ring focus:ring-stone-600"
-            disabled={isLoading}
-          />
-        </div>
-        <div className="flex flex-col gap-2 mt-2 mb-4">
-          <label className="text-stone-700">Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="***********"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-stone-200 px-1 py-2 rounded-md outline-none focus:ring focus:ring-stone-600"
-            disabled={isLoading}
-          />
-        </div>
-        <Button type="stretch" disabled={isLoading}>
-          {isLoading ? "Authenticating..." : "Login"}
-        </Button>
-      </form>
+
+      <LoginForm />
     </div>
   );
 }
