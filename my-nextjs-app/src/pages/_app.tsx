24f43b3eb0b5bced81d9897d220eb572ed858989
UmarFarukA <talk2ufaz@gmail.com>
2024-07-19 12:13:02 +0100

    Initial commit

--- my-nextjs-app/src/pages/_app.tsx
+++ my-nextjs-app/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <header className="p-4 flex items-center gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/posts">Posts</Link>
+        <Link href="/about">About</Link>
+      </header>
+      <div className="p-4">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
